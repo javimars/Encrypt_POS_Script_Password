4bf167f1fd409f13ff170850bced817c30d67cf5
Javier Tapia <Javimars3000@yahoo.com>
2023-02-22 23:36:30 -0500

    .

--- Encrypt_POS_Script_Password/FileEncryptor.cs
+++ Encrypt_POS_Script_Password/FileEncryptor.cs
@@ -3,20 +3,27 @@ using System.Text.RegularExpressions;
 
 namespace Encrypt_POS_Script_Password;
 
-
-public class FileEncryptor
+public partial class FileEncryptor
 {
-    private readonly byte[] key = new byte[0]
-    {
-        // Key byte array   to encrypt password 
-        
-        /* Your secret key here */
-    };
+    private readonly byte[] iv;
+    private readonly byte[] key;
 
-    private readonly byte[] iv = new byte[0]
+    public FileEncryptor()
     {
-        /* Your secret IV here */
-    };
+        // Generate a new 256-bit key
+        using (var aes = Aes.Create())
+        {
+            aes.KeySize = 256;
+            key = aes.Key;
+        }
+
+        // Generate a new 128-bit IV
+        using (var aes = Aes.Create())
+        {
+            aes.KeySize = 128;
+            iv = aes.IV;
+        }
+    }
 
     public void EncryptPasswordInPOSIniFiles(string drive)
     {
@@ -41,63 +48,51 @@ public class FileEncryptor
     private void EncryptPasswordInFile(string filePath)
     {
         var fileContent = File.ReadAllText(filePath);
-        var pattern = @"(?<=Password=).*?(?=\[Micros Settings\])";
-        var match = Regex.Match(fileContent, pattern, RegexOptions.Singleline);
+        const string pattern = @"(?<=Password=).*?(?=\[Micros Settings\])";
+        var match = MyRegex().Match(fileContent);
 
-        if (match.Success)
-        {
-            var password = match.Value.Trim();
-            var encryptedPassword = EncryptStringToBytes_Aes(password);
+        if (!match.Success) return;
+        var password = match.Value.Trim();
+        var encryptedPassword = EncryptStringToBytes_Aes(password);
 
-            var encryptedPasswordString = Convert.ToBase64String(encryptedPassword);
-            var newFileContent = Regex.Replace(fileContent, pattern, encryptedPasswordString);
+        var encryptedPasswordString = Convert.ToBase64String(encryptedPassword);
+        var newFileContent = MyRegex3().Replace(fileContent, encryptedPasswordString);
 
-            File.WriteAllText(filePath, newFileContent);
-        }
+        File.WriteAllText(filePath, newFileContent);
     }
 
     private void DecryptPasswordInFile(string filePath)
     {
         var fileContent = File.ReadAllText(filePath);
-        var pattern = @"(?<=Password=).*?(?=\[Micros Settings\])";
-        var match = Regex.Match(fileContent, pattern, RegexOptions.Singleline);
+        const string pattern = @"(?<=Password=).*?(?=\[Micros Settings\])";
+        var match = MyRegex1().Match(fileContent);
 
-        if (match.Success)
-        {
-            var encryptedPasswordString = match.Value.Trim();
-            var encryptedPassword = Convert.FromBase64String(encryptedPasswordString);
-            var password = DecryptStringFromBytes_Aes(encryptedPassword);
+        if (!match.Success) return;
+        var encryptedPasswordString = match.Value.Trim();
+        var encryptedPassword = Convert.FromBase64String(encryptedPasswordString);
+        var password = DecryptStringFromBytes_Aes(encryptedPassword);
 
-            var newFileContent = Regex.Replace(fileContent, pattern, password);
+        var newFileContent = MyRegex2().Replace(fileContent, password);
 
-            File.WriteAllText(filePath, newFileContent);
-        }
+        File.WriteAllText(filePath, newFileContent);
     }
 
     private byte[] EncryptStringToBytes_Aes(string plainText)
     {
-        byte[] encrypted;
+        using var aesAlg = Aes.Create();
+        aesAlg.Key = key;
+        aesAlg.IV = iv;
 
-        using (var aesAlg = Aes.Create())
+        var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+        using var msEncrypt = new MemoryStream();
+        using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+        using (var swEncrypt = new StreamWriter(csEncrypt))
         {
-            aesAlg.Key = key;
-            aesAlg.IV = iv;
-
-            var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-            using (var msEncrypt = new MemoryStream())
-            {
-                using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                {
-                    using (var swEncrypt = new StreamWriter(csEncrypt))
-                    {
-                        swEncrypt.Write(plainText);
-                    }
-
-                    encrypted = msEncrypt.ToArray();
-                }
-            }
+            swEncrypt.Write(plainText);
         }
+
+        var encrypted = msEncrypt.ToArray();
 
         return encrypted;
     }
@@ -106,24 +101,29 @@ public class FileEncryptor
     {
         string plaintext = null;
 
-        using (var aesAlg = Aes.Create())
-        {
-            aesAlg.Key = key;
-            aesAlg.IV = iv;
+        using var aesAlg = Aes.Create();
+        aesAlg.Key = key;
+        aesAlg.IV = iv;
 
-            var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+        var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-            using (var msDecrypt = new MemoryStream(cipherText))
-            {
-                using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                {
-                    using (var srDecrypt = new StreamReader(csDecrypt))
-                    {
-                        plaintext = srDecrypt.ReadToEnd();
-                    }
-                }
-            }
-        }
-        return plaintext; }
-    
+        using var msDecrypt = new MemoryStream(cipherText);
+        using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+        using var srDecrypt = new StreamReader(csDecrypt);
+        plaintext = srDecrypt.ReadToEnd();
+
+        return plaintext;
+    }
+
+    [GeneratedRegex("(?<=Password=).*?(?=\\[Micros Settings\\])", RegexOptions.Singleline)]
+    private static partial Regex MyRegex();
+
+    [GeneratedRegex("(?<=Password=).*?(?=\\[Micros Settings\\])", RegexOptions.Singleline)]
+    private static partial Regex MyRegex1();
+
+    [GeneratedRegex("(?<=Password=).*?(?=\\[Micros Settings\\])")]
+    private static partial Regex MyRegex2();
+
+    [GeneratedRegex("(?<=Password=).*?(?=\\[Micros Settings\\])")]
+    private static partial Regex MyRegex3();
 }
