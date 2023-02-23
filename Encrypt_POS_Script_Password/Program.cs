a6231407284d86eff339477b1903fd910cd68513
Javier Tapia <Javimars3000@yahoo.com>
2023-02-23 09:55:10 -0500

    .

--- Encrypt_POS_Script_Password/Program.cs
+++ Encrypt_POS_Script_Password/Program.cs
@@ -1,21 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Encrypt_POS_Script_Password;
-
 public class Program
 {
     public static void Main(string[] args)
     {
         var path = Console.ReadLine();
-
-        if (File.Exists(path))
-            // This path is a file
-            RecursiveFileProcessor.ProcessFile(path);
-        else if (Directory.Exists(path))
-            // This path is a directory
-            RecursiveFileProcessor.ProcessDirectory(path);
-        else
-            Console.WriteLine("{0} is not a valid file or directory.", path);
     }
 }
 
