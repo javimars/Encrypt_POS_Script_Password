78ab20c652bc9a1ec164b49ea8d5f4d313082243
Javier Tapia <Javimars3000@yahoo.com>
2023-02-23 08:01:53 -0500

    .

--- Encrypt_POS_Script_Password/Program.cs
+++ Encrypt_POS_Script_Password/Program.cs
@@ -2,6 +2,23 @@
 
 using Encrypt_POS_Script_Password;
 
-Console.WriteLine("Hello, World!");
-var encryptor = new FileEncryptor();
-encryptor.EncryptPasswordInPOSIniFiles("d:\\");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var path = Console.ReadLine();
+
+        if (File.Exists(path))
+            // This path is a file
+            RecursiveFileProcessor.ProcessFile(path);
+        else if (Directory.Exists(path))
+            // This path is a directory
+            RecursiveFileProcessor.ProcessDirectory(path);
+        else
+            Console.WriteLine("{0} is not a valid file or directory.", path);
+    }
+}
+
+
+// encryptor = new FileEncryptor();
+//encryptor.EncryptPasswordInPOSIniFiles("e:\\");
---
+++ Encrypt_POS_Script_Password/RecursiveFileProcessor.cs
@@ -0,0 +1,25 @@
+﻿namespace Encrypt_POS_Script_Password;
+
+public class RecursiveFileProcessor
+{
+    // Process all files in the directory passed in, recurse on any directories
+    // that are found, and process the files they contain.
+    public static void ProcessDirectory(string targetDirectory)
+    {
+        // Process the list of files found in the directory.
+        var fileEntries = Directory.GetFiles(targetDirectory);
+        foreach (var fileName in fileEntries)
+            ProcessFile(fileName);
+
+        // Recurse into subdirectories of this directory.
+        var subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+        foreach (var subdirectory in subdirectoryEntries)
+            ProcessDirectory(subdirectory);
+    }
+
+    // Insert logic for processing found files here.
+    public static void ProcessFile(string path)
+    {
+        Console.WriteLine("Processed file '{0}'.", path);
+    }
+}
